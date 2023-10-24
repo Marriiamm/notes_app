5a668e9e4cf8222087a5b550f642f9da32703678
Marriiamm <marriiamm453@gmail.com>
2023-10-24 03:44:50 +0300

    create notesView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -9,6 +10,12 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home:const NotesView(),
+    );
   }
 }
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text("Notes",style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+}
